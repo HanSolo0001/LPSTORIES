1024c07f6b876b71ab6de87ebbe363d599027d6f
HanSolo0001 <pillarite@gmail.com>
2015-06-07 13:35:52 -0400

    Changed files to the updated website

---
+++ scripts.js
@@ -0,0 +1,7 @@
+/*
+if (document.cookie.replace(/(?:(?:^|.*;\s*)underConstruction\s*\=\s*([^;]*).*$)|^.*$/, "$1") !== "true") {
+  alert("This site is still under construction. Leave feedback by clicking the webmasters link at the bottom of the page.");
+  document.cookie = "underConstructione=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
+}
+*/
+
