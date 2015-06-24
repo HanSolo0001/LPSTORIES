14e1fe1bb47fcc769da04772a782e590312247f6
HanSolo0001 <pillarite@gmail.com>
2015-06-24 19:56:24 -0400

    Adjusted files for a more flowing look

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
-/*
-Gallery Lightbox On Stories Page
-*/
+/*************************************************************
+            Gallery Lightbox On Stories Page
+**************************************************************/
 
 //Problem: User when clicking on image goes to a dead end
 //Solution: Create an overlay with the large image - Lightbox
@@ -40,8 +40,40 @@ $overlay.click(function(){
   $overlay.hide();
 });
   
+/********************************************
+            Simple Drop Down Menu
+********************************************/
 
-
+////Problem: It looks gross in smaller browser widths and small devices
+////Solution: To hide the text links and swap them out with a more appropriate navigation
+//
+////Create a select and apend to menu
+//var $select = $("<select></select>");
+//$("#menu").append($select);
+//
+////Cycle over menu links
+//$("#menu a").each(function(){
+//  var $anchor = $(this);
+//  //Create an option
+//  var $option = $("<option></option>");
+//
+//  //Deal with selected options depending on current page
+//  if($anchor.parent().hasClass("selected")) {
+//    $option.prop("selected", true);
+//  }
+//  //option's value is the href
+//  $option.val($anchor.attr("href"));
+//  //option's text is the text of link
+//  $option.text($anchor.text());
+//  //append option to select
+//  $select.append($option);
+//});
+//
+////Bind change listener to the select
+//$select.change(function(){
+//  //Go to select's location
+//  window.location = $select.val();
+//});
 
 
 
